acfdd64bf2b38499851846a270fbeb1a6e09336f
Raman <ramanmcreates@gmail.com>
2022-10-24 11:21:03 -0400

    new

---
+++ code.js
@@ -0,0 +1,10 @@
+fetch("https://www.balldontlie.io/api/v1/players")
+    .then((res) => res.json())
+    .then((data) => displayPlayers(data))
+
+function displayPlayers(data){
+    data.forEach( data {
+        let playerName = document.createElement("h1")
+
+    })
+}
