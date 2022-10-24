367f4b60f604283e7f19ab1d9e6012cfc9d6c204
Raman <ramanmcreates@gmail.com>
2022-10-24 15:52:52 -0400

    commit message

--- code.js
+++ code.js
@@ -1,10 +1,57 @@
-fetch("https://www.balldontlie.io/api/v1/players")
+//Define Variables
+const head = document.getElementById("header")
+
+async function fetchStuff(){
+
+await fetch("https://www.balldontlie.io/api/v1/teams")
+    .then((res) => res.json())
+    .then((data) => renderTeams(data)) 
+await fetch("https://www.balldontlie.io/api/v1/players?per_page=100")
+    .then((res) => res.json())
+    .then((data) => displayPlayers(data))
+await fetch("https://www.balldontlie.io/api/v1/players?per_page=100&page=2")
+    .then((res) => res.json())
+    .then((data) => displayPlayers(data))
+await fetch("https://www.balldontlie.io/api/v1/players?per_page=100&page=3")
+    .then((res) => res.json())
+    .then((data) => displayPlayers(data))
+await fetch("https://www.balldontlie.io/api/v1/players?per_page=100&page=4")
     .then((res) => res.json())
     .then((data) => displayPlayers(data))
 
-function displayPlayers(data){
-    data.forEach( data {
-        let playerName = document.createElement("h1")
+}
 
+
+fetchStuff()
+
+
+
+//First Fetch
+function renderTeams(data){
+    console.log(data)
+    data.data.forEach(data => {
+        let teamHeader = document.createElement("h1")
+        let teamContainer = document.createElement("div")
+        teamContainer.id = data.name
+        teamHeader.innerText = data.name
+        teamContainer.append(teamHeader)
+        head.append(teamContainer)    
     })
+
+}
+
+
+//Second Fetch 
+function displayPlayers(data){
+    console.log(data)
+    data.data.forEach( data => {
+        let playerName = document.createElement("h3")
+        playerName.innerHTML  =  `${data.first_name}` +  " " + `${data.last_name}`
+        document.getElementById(data.team.name).append(playerName)
+    })
+}
+
+
+function displayYear(data){
+    console.log(data)
 }
